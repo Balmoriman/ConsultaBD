1e9489be29cdc4225cdb2ded70c275d7f8bf5c56
Balmoriman <alexthestinger@gmail.com>
2019-05-31 16:55:18 -0500

    init

---
+++ TestDB/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TestDB.Models;
+
+namespace TestDB
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Lectura(); 
+        }
+
+        private ObservableCollection<VM> datosCosnulta = new ObservableCollection<VM>();
+        public ObservableCollection<VM> DatosConsulta => datosCosnulta; 
+
+        public List<VM> Consulta()
+        {
+            using(DSNet3_DesarrolloContext  conn = new DSNet3_DesarrolloContext())
+            {
+                var consulta = conn.Calendario.Select(s => new VM
+                {
+                    Fecha = s.Fecha,
+                    Descripcion = s.Descripcion,
+                    ClaveEstatus = s.ClaveEstatus,
+                    Estatus = s.ClaveEstatus == 1 ? "Activo" : "inactivo",
+                    FechaAlta = s.FechaAlta,
+                    UsuarioAlta = s.UsuarioAlta
+                }).OrderBy(o => o.FechaAlta);
+
+                return consulta.ToList();
+            }
+      
+        }
+
+        public void Lectura()
+        {
+            //foreach(var item in Consulta())
+            //{
+            //    datosCosnulta.Add(item);
+            //}
+
+            dgGrid.ItemsSource = Consulta();
+            
+        }
+       
+    }
+}
---
+++ TestDB/VM.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TestDB
+{
+    [Serializable]
+    public class VM 
+    {
+
+       // public int Fecha { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Descripcion { get; set; }
+        public int ClaveEstatus { get; set; }
+        public string Estatus { get; set; }
+        public int UsuarioAlta { get; set; }
+        public DateTime FechaAlta { get; set; }
+        //public string MyProperty 
+        //{
+        //    get { return myVar; }
+        //    set { myVar = value; }
+        //}
+
+
+        //public string MyProperty
+        //{
+        //    get { return myVar; }
+        //    set { myVar = value; }
+        //}
+
+
+        //public string MyProperty
+        //{
+        //    get { return myVar; }
+        //    set { myVar = value; }
+        //}
+
+        //private int myVar;
+
+        //public int MyProperty
+        //{
+        //    get { return myVar; }
+        //    set { myVar = value; }
+        //}
+
+
+
+        //public DateTime MyProperty
+        //{
+        //    get { return myVar; }
+        //    set { myVar = value; }
+        //}
+
+
+        //private int myVar;
+
+        //public int MyProperty
+        //{
+        //    get { return myVar; }
+        //    set { myVar = value; }
+        //}
+
+        //private int myVar;
+
+        //public int MyProperty
+        //{
+        //    get { return myVar; }
+        //    set { myVar = value; }
+        //}
+
+
+        //private int myVar;
+
+        //public int MyProperty
+        //{
+        //    get { return myVar; }
+        //    set { myVar = value; }
+        //}
+
+        //private int myVar;
+
+        //public int MyProperty
+        //{
+        //    get { return myVar; }
+        //    set { myVar = value; }
+        //}
+
+
+
+    }
+}
